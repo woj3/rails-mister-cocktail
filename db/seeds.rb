96adec94fd2a5d33fcf8e163c5dd00722265b893
Dominik Wojciechowski <dominik.woj@me.com>
2019-08-15 14:40:18 +0200

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "lime")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Rum")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Whiskey")
+Ingredient.create(name: "Tequila")
+Ingredient.create(name: "BRandy")
+Ingredient.create(name: "eggs")
+Ingredient.create(name: "oranges")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "Juice")
+Ingredient.create(name: "vermouth")
+
+
+
